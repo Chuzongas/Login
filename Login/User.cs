8f4442219eb69daba1d6908f0575ad661987cdfc
Chuz <jesusalonsovara@hotmail.com>
2018-09-04 11:26:19 -0600

    Agregar variables a clase User

--- Login/User.cs
+++ Login/User.cs
@@ -7,6 +7,22 @@ using System.Threading.Tasks;
 namespace Login
 {
     class User
+
     {
+
+        public int Id { get; set; }
+
+        public string Name { get; set; }
+
+        public string Password { get; set; }
+
+        public string FirstName { get; set; }
+
+        public string LastName { get; set; }
+
+        public string Email { get; set; }
+
+        public string RememberToken { get; set; }
+
     }
 }
